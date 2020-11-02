77ae1317ab874857386226f97171f35fe0190e58
pyhan23@gmail.com <pyhan23@gmail.com>
2020-11-02 14:28:25 +0900

    11/2

--- src/component/Ntwitt.js
+++ src/component/Ntwitt.js
@@ -1,23 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { dbService } from "fbase";
 
 const Ntwitt = ({ JHtwittObj, isOwner}) =>{ 
-  const onDeleteClick = () => {
-    const ok = window.confirm("Are you suer you want to delete this?");
+  const [Editing, setEditing] = useState(false);
+  const [Newtwitt, setNewtwitt] = useState(JHtwittObj.text);
+  const onDeleteClick = async() => {
+    const ok = window.confirm("Are you sure?");
     if(ok){
-      //delete twitt
-      
+      await dbService.doc(`JHtwitts/${JHtwittObj.id}`).delete();
     }
-  }
+  };
+  const toggleEditing =() => setEditing((prev) => !prev);
   return(
-      <div>
-      <h4>{JHtwittObj.text}</h4>
-    {isOwner && (
-    <>
-    <button>Delete Twitt</button>
-      <button>Edit Twitt</button>
+  <div>
+    {Editing ? (
+      <>
+      <form>
+        <input 
+        type ="text" 
+        placeholder="Edit this" 
+        value = {Newtwitt} 
+        required 
+        />
+      </form>
+      <button onClick ={toggleEditing}>Cancel</button>
       </>
-      )}
-    </div>
+    ) : (
+      <>
+        <h4>{JHtwittObj.text}</h4>
+        {isOwner && (
+          <>
+          <button onClick={onDeleteClick}>Delete Twitt</button>
+          <button onClick={toggleEditing}>Edit Twitt</button>
+          </>
+        )}
+      </>
+     )}
+  </div>
   );
 };
+
 export default Ntwitt;
